52a88a9191ce283e1ff82d2b397a34c2347f86e2
MonikaChris <monidavies@gmail.com>
2022-10-06 14:18:04 -0700

    Adds parse_template function

--- madlib.py
+++ madlib.py
@@ -1,7 +1,11 @@
+import re
+
 # Variables
 width = 52
 margin = '**'
-messages = ['Welcome! Let\'s play MadLibs!', 'Enter a noun, verb, or adjective when propmted.', 'Enter "quit" to exit.']
+messages = ['Welcome! Let\'s play MadLibs!',
+            'Enter a noun, verb, or adjective when propmted.',
+            'Enter "quit" to exit.']
 playing = True
 
 # Welcome user
@@ -10,23 +14,37 @@ for message in messages:
     space = width - len(message) - (2 * len(margin))
 
     if space % 2 == 0:
-        print(margin + ' ' * (space//2) + message + ' ' * (space//2) + margin)
+        print(
+            margin + ' ' * (space // 2) + message + ' ' * (space // 2) + margin)
     else:
-        print(margin + ' ' * (space//2) + message + ' ' * (space//2 + 1) + margin)
+        print(margin + ' ' * (space // 2) + message + ' ' * (
+                    space // 2 + 1) + margin)
 print('*' * width)
+
 
 def read_template(file):
     with open(file, 'r') as reader:
         return reader.read()
 
-def parse_template(text):
-    pass
 
+def parse_template(text):
+    pattern = r"[{]\w+[}]"
+
+    stripped = re.sub(pattern, '{}', text)
+
+    parts_list = re.findall(pattern, text)
+    parts = []
+
+    for part in parts_list:
+        parts.append(part[1:-1])
+
+    return stripped, tuple(parts)
 
 def merge():
     pass
 
 
-#while playing:
+# while playing:
 
 
+print(parse_template("It was a {Adjective} and {Adjective} {Noun}."))
